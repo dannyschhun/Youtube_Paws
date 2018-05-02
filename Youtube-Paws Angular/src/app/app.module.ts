8e45737efd780495228b562c908f08f552c848c3
danny <dannyschhun@gmail.com>
2018-05-02 13:18:39 -0400

    Changes

--- Youtube-Paws Angular/src/app/app.module.ts
+++ Youtube-Paws Angular/src/app/app.module.ts
@@ -2,24 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RoutingModule } from './routing.module';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { UserService } from './services/user.service';
+import { NavComponent } from './components/nav/nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RoutingModule
+    RoutingModule,
+    CollapseModule.forRoot()
+  ],
+  exports: [
+    CollapseModule
   ],
   providers: [
     UserService
