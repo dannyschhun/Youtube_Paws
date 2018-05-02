2d1a28df2c90c7f9169cb6353f9a53a5c0296457
danny <dannyschhun@gmail.com>
2018-05-02 17:47:21 -0400

    Revert "Revert "Merge pull request #3 from dannyschhun/Danny_C""
    This reverts commit 5f8d43fbbace997388cd44ca5fd5bd96c29935ef.

---
+++ Youtube-Paws Angular/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RoutingModule } from './routing.module';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UserService } from './services/user.service';
+import { NavComponent } from './components/nav/nav.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RoutingModule,
+    CollapseModule.forRoot()
+  ],
+  exports: [
+    CollapseModule
+  ],
+  providers: [
+    UserService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
