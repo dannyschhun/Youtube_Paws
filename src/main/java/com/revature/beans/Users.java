76da78a0ba36ec7ce42d5e997f6cb60b65b78b49
Byron Hall <byronhall020@gmail.com>
2018-05-10 14:12:08 -0400

    changed eager view setting fetch to lazy
    in the Users Bean

--- src/main/java/com/revature/beans/Users.java
+++ src/main/java/com/revature/beans/Users.java
@@ -33,8 +33,8 @@ public class Users implements Serializable{
 	@Column(name = "USER_RANK")
 	private Integer rank;
 
-	
-	@ManyToMany(cascade=CascadeType.ALL,fetch=FetchType.EAGER)
+	//changing fetch type to lazy
+	@ManyToMany(cascade=CascadeType.ALL,fetch=FetchType.LAZY)
 	@JoinTable(
 			name="USERS_VS",
 			joinColumns=@JoinColumn(name="USERS_ID"),
