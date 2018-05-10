6589673c00b48680092afc2c4d805f9911816740
Byron Hall <byronhall020@gmail.com>
2018-05-10 09:57:22 -0400

    javadoc

--- src/main/java/com/revature/repositories/HistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.revature.model.History;
-
-@Repository
-public interface HistoryRepository extends JpaRepository<History, Integer>{}
--- src/main/java/com/revature/repositories/PageLayoutRepository.java
+++
@@ -1,7 +0,0 @@
-package com.revature.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.revature.model.PageLayout;
-
-public interface PageLayoutRepository extends JpaRepository<PageLayout, String>{}
